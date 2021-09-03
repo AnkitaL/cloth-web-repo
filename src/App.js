f0ab1ac3a5e77383c25de763025e1e666fbbbf86
AnkitaL <lakhaniankita0@gmail.com>
2021-09-03 15:38:23 +0530

    add shop and signin component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import { Switch,Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SignInAndSignOut from './pages/signIn-and-signout/signin-and-signout.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignOut} />
       </Switch>
     </div>
   );
